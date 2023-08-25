1181fe72082a57bad9b2b2a7d3caff6237b5a26f
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-08-25 16:41:10 +0100

    Change to process.env.ENS_NAME

--- scripts/createTextRecord.ts
+++ scripts/createTextRecord.ts
@@ -2,8 +2,8 @@ import { dnsEncode } from "ethers/lib/utils";
 import hre from "hardhat";
 import { L2PublicResolver__factory } from "typechain";
 
-const l2ResolverAddress = "0xc1C2b9dD2D15045D52640e120a2d1F16dA3bBb48";
-const ENS_NAME = "bob123.eth";
+const l2ResolverAddress = process.env.L2_RESOLVER_ADDRESS
+const ENS_NAME = process.env.ENS_NAME
 
 export async function createTextRecord() {
     const L2PublicResolverFactory = (await hre.ethers.getContractFactory("L2PublicResolver")) as L2PublicResolver__factory;
